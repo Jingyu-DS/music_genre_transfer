1884d39d20b161e7ff521354ff84a1f2c7d9f22b
Zuxiang Shang <123657978+Zuxiang-Shang@users.noreply.github.com>
2025-05-05 01:35:33 -0400

    Update image_final_loss.py

--- image_final_loss.py
+++ image_final_loss.py
@@ -6,19 +6,19 @@ import torch.nn.functional as F
 class EfficientLoss(nn.Module):
     def __init__(self, n_mels=128):
         super().__init__()
-        w = torch.ones(1,1,n_mels,1)
-        w[:,:,30:80,:] = 1.3
-        self.register_buffer('freq_weights', w)
-
+        self.register_buffer('freq_weights', torch.ones(1, 1, n_mels, 1))
+        self.freq_weights[:, :, 30:80, :] = 1.3
     def forward(self, x, recon, mean, logvar, beta):
-        weighted_mse = torch.mean(self.freq_weights * (x-recon)**2)
-        xg = x[:,:,:,1:] - x[:,:,:,:-1]
-        rg = recon[:,:,:,1:] - recon[:,:,:,:-1]
-        grad_loss = F.l1_loss(xg, rg)
-        recon_loss = 0.7*weighted_mse + 0.3*grad_loss
-        m_low,  m_high  = torch.chunk(mean,  2, dim=1)
-        lv_low, lv_high = torch.chunk(logvar,2, dim=1)
-        kl_low  = -0.5*torch.mean(1+lv_low - m_low.pow(2) - lv_low.exp())
-        kl_high = -0.5*torch.mean(1+lv_high- m_high.pow(2)- lv_high.exp())
-        kl = kl_low + 0.5*kl_high
-        return recon_loss + beta*kl, recon_loss, kl
+        weighted_mse = torch.mean(self.freq_weights * (x - recon)**2)
+        x_grad_t = x[:, :, :, 1:] - x[:, :, :, :-1]
+        recon_grad_t = recon[:, :, :, 1:] - recon[:, :, :, :-1]
+        grad_loss = F.l1_loss(x_grad_t, recon_grad_t)
+        recon_loss = 0.7 * weighted_mse + 0.3 * grad_loss
+        mean_low, mean_high = torch.chunk(mean, 2, dim=1)
+        logvar_low, logvar_high = torch.chunk(logvar, 2, dim=1)
+        kl_low = -0.5 * torch.mean(1 + logvar_low - mean_low.pow(2) - logvar_low.exp())
+        kl_high = -0.5 * torch.mean(1 + logvar_high - mean_high.pow(2) - logvar_high.exp())
+        kl = kl_low + 0.5 * kl_high
+        total_loss = recon_loss + beta * kl
+        
+        return total_loss, recon_loss, kl
